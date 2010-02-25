bc11bc5a06776593fe65c9ce2f56a201e45b8d9e
Aaron Craelius <aaronc+git@practivist.org>
2010-02-25 17:23:00 -0500

    Fixed bug in stuff_to_do_filter.rb

--- app/models/stuff_to_do_filter.rb
+++ app/models/stuff_to_do_filter.rb
@@ -27,7 +27,9 @@ class StuffToDoFilter
   private
   # Wrapper around Redmine's API since Enumerations changed in r2472
   def get_priorites
-    if Enumeration.respond_to?(:priorities)
+    if IssuePriority.respond_to?(:all)
+      return IssuePriority.all
+    elsif Enumeration.respond_to?(:priorities)
       return Enumeration.priorities
     else
       return Enumeration::get_values('IPRI')
